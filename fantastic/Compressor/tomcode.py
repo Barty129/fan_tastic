5a747d8620adb26e0c27132ffd43baabe7d41d3d
Lopez304 <tom_lopez@hotmail.co.uk>
2023-05-19 23:08:41 +0100

    Fixed rotor plotter, added diffuser code

--- fantastic/Compressor/tomcode.py
+++ fantastic/Compressor/tomcode.py
@@ -1,15 +1,17 @@
+from re import L
 import numpy as np
 import matplotlib.pyplot as plt
 
 # design parameters to be altered
-beta2 = -1 # trailing edge blade angle in radians
-r1 = 0.03 # leading edge radius
+beta2 = -0.75 # trailing edge blade angle in radians
+r1 = 0.05 # leading edge radius
 inletangle = 5 # angle of incidence of leading edge in degrees
 vaneless = 1.1 # ratio of diffuser leading edge to rotor trailing edge radii
 arearatio = 3
 aspectratio = 10
 # numerical parameters
 step = 0.001
+r4 = 0.15
 
 # design point values
 nc = 0.6
@@ -26,15 +28,18 @@ def bladeplot(N, r1, r2, step, beta1, beta2):
     theta = [[0 for j in range(len(r))] for i in range(N)]
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     beta = [0 for i in range(len(r))]
-    c = np.tan(beta2)*2/r2**2
-    for i in range(len(r)):
-        beta[i] = -1*np.arctan(c*r[i]**2/2)
-    for i in range(0, N):   
-        for j in range(0, len(r)):
-            theta[i][j] = i * 2 * np.pi/N + np.tan(beta[j])*np.log(r[j]) - np.tan(beta[0])*np.log(r[0])
+    c = (np.tan(beta1) - np.tan(beta2))/(r1**2/2 - r2**2/2)
+    d = np.tan(beta2) - c * r2 ** 2/2
+    for j in range(len(r)):
+        beta[j] = 1*np.arctan(c*r[j]**2/2 + d)
+    for i in range(0, N): 
+        theta[i][0] = i * 2 * np.pi/N
+        for j in range(1, len(r)):
+            theta[i][j] = theta[i][j-1] + np.tan(beta[j])/r[j] * step
         ax.plot(theta[i], r)
     ax.grid(True)
     plt.show()
+    return(r, theta)
 
 def velocitycalc(N = 0):
     # calculates velocity triangles
@@ -72,6 +77,59 @@ def bladenumber():
     Nb = mdot * drVthetadr / (2 * density * Wavg ** 2 * rmid * h)
     return(Nb)
 
+def throatspace(r, theta):
+    # finds minimum throat area in (ie min distance from any point to any point)
+    mindist = 1000
+    for i in range(0, len(r)):
+        for j in range(0, len(r)):
+            x1 = r[i] * np.cos(theta[0][i])
+            y1 = r[i] * np.sin(theta[0][i])
+            x2 = r[j] * np.cos(theta[1][j])
+            y2 = r[j] * np.sin(theta[1][j])
+            dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) **  0.5
+            if dist < mindist:
+                mindist = dist
+    minarea = mindist * h
+    return minarea
+
+def maxthroat(r, theta):
+    # finds throat area maximum (ie min distance of any point to trailing edge)
+    mindist = 1000
+    for j in range(0, len(r)):
+        x1 = r[-1] * np.cos(theta[0][-1])
+        y1 = r[-1] * np.sin(theta[0][-1])
+        x2 = r[j] * np.cos(theta[1][j])
+        y2 = r[j] * np.sin(theta[1][j])
+        x3 = r[j] * np.cos(theta[-1][j]) # Extra point accounts for both forward sweep and backwards sweep
+        y3 = r[j] * np.sin(theta[-1][j])
+        dist1 = ((x2 - x1) ** 2 + (y2 - y1) ** 2) **  0.5
+        dist2 = ((x3 - x1) ** 2 + (y3 - y1) ** 2) **  0.5
+        if dist1 < mindist:
+            mindist = dist1
+        if dist2 < mindist:
+            mindist = dist2
+    minarea = mindist * h
+    return minarea
+
+        
+def diffuserbladeplot(N, r1, r2, step, beta1, beta2):
+    # plots diffuser blade shapes
+    r = np.arange(r1, r2, 0.001)
+    theta = [[0 for j in range(len(r))] for i in range(N)]
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    beta = [0 for i in range(len(r))]
+    for j in range(len(r)):
+        beta[j] = beta1 + (beta2 - beta1) * (r[j] - r1)/(r2 - r1)
+    for i in range(0, N): 
+        theta[i][0] = i * 2 * np.pi/N
+        for j in range(1, len(r)):
+            theta[i][j] = theta[i][j-1] + np.tan(beta[j])/r[j] * step
+        ax.plot(theta[i], r)
+    ax.grid(True)
+    plt.show()
+    return(r, theta)
+
+
 Vr2, omega, Vtheta1, Vtheta2, Vr1, alphaRel1, beta1, Vrel1, Vrel2 = velocitycalc()
 N = bladenumber()
 N = round(N * 1.25)
@@ -82,4 +140,10 @@ bladeplot(N, r1, r2, step, beta1, beta2)
 r3 = r2 * vaneless
 Vr3 = Vr2 * r2/r3
 Vtheta3 = Vtheta2 * r2/r3
-beta1 = -1 * np.arctan(Vtheta3/Vr3)
+betad1 = np.arctan(Vtheta3/Vr3)
+betad2 = 1.1
+
+r, theta = diffuserbladeplot(10, r3, r4, step, betad1, betad2)
+minarea = throatspace(r, theta)
+exitarea = maxthroat(r, theta)
+print("Area ratio is", exitarea/minarea)
